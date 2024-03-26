0b23f062566ec5c0d8c18299f82e7e61f56295f6
forestario <forestar.hot@gmail.com>
2024-03-27 05:11:50 +0800

    chore: update tailwind config for preset colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,41 @@ const _ = require('lodash');
 const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
 
 module.exports = {
-  content: ['./src/**/*.tsx', './src/**/*.css'],
+  content: ['./src/**/*.tsx', './src/**/*.{css, scss}'],
   darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#fff',
+        black: '#000',
+        grey: {
+          0: '#ffffff',
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#f0f0f0',
+          300: '#d9d9d9',
+          400: '#bfbfbf',
+          500: '#8c8c8c',
+          600: '#595959',
+          700: '#262626',
+          800: '#141414',
+          900: '#000000',
+        },
+      },
+      textColor: {
+        primary: `rgba(255, 255, 255, 0.87)`,
+        secondary: `rgba(255, 255, 255, 0.45)`,
+      },
+      divideColor: `rgba(255, 255, 255, 0.1)`,
+      borderRadius: {
+        primary: '4px',
+        secondary: '8px',
+        regular: '50%',
+      },
+    },
+  },
   plugins: [
     function ({ addUtilities, e, theme, variants }) {
       const colors = flattenColorPalette(theme('borderColor'));
