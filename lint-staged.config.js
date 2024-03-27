b962ce571317ec6b1ffe2fc04b6bc17aa4200d3d
forestario <forestar.hot@gmail.com>
2024-03-28 05:32:28 +0800

    chore: minor update in eslint config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts,tsx,css}': ['prettier . --write'],
+  '*.{ts,tsx,scss}': ['prettier . --write'],
   '*.{ts,tsx}': [
     'eslint . --cache --fix --ext .tsx --ext .ts',
     () => 'yarn tsc',
